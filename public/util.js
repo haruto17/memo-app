d877d58a3f10c50b46e2a9840f25fbc1c558e1e4
haruto17 <s1902272@sendai-nct.jp>
2023-03-27 11:36:04 +0900

    リストに追加されるメモに絵文字が表示されるように変更

--- public/util.js
+++ public/util.js
@@ -86,10 +86,14 @@ function appendMemo(key) {
     div.className = "memo";
     div.id = key;
     div.setAttribute("onclick", "clickMemo(event)");
+    const icon = document.createElement("p");
+    icon.innerText = memo[0];
+    icon.className = "memoIcon";
     const line = document.createElement("hr");
     const titleText = document.createElement("p");
     titleText.innerText = memo[1];
     titleText.className = "title-text";
+    div.appendChild(icon);
     div.appendChild(line);
     div.appendChild(titleText);
     list.appendChild(div);
